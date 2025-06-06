a8dbff5bca31ca77d7532904dc7de941fcab2c64
ikotsov <i.kotsovilis@gmail.com>
2025-06-06 20:55:16 +0300

    feat: run verpart

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include "horpart/horpart.h"
 #include "horpart/utility_constraints.h"
 #include "horpart/types.h"
+#include "verpart/verpart.h"
 
 const std::string DATASET_FILE = "data/dataset.csv";
 const std::string CONSTRAINTS_FILE = "data/utility_constraints.json";
 
 const size_t k = 3;
 const size_t MAX_CLUSTER_SIZE = 2 * k;
+const size_t m = 2;
 
 int main()
 {
@@ -30,6 +32,17 @@ int main()
         }
         std::cout << "-------------------\n";
     }
+
+    std::cout << "HorPart produced " << clusters.size() << " clusters.\n";
+
+    std::vector<VerPartOutput> verpart_results;
+    for (const auto &cluster : clusters)
+    {
+        VerPartOutput vp = VerPart(cluster, U, k, m);
+        verpart_results.push_back(vp);
+    }
+
+    exportToJson(verpart_results, "data/toy_disassociated_output.json");
 
     return 0;
 }
